8511a24f8e3ef8f4b46e203f76457c9f26759916
Eelis Tirroniemi <124739749+MatkallaPankkiin@users.noreply.github.com>
2023-08-20 22:51:38 +0300

    farssi

--- server/server.js
+++ server/server.js
@@ -22,15 +22,14 @@ app.post('/', async(req, res) =>{
     try {
         const prompt=req.body.prompt;
         
-        const response = await openai.createCompletion.create({
+        const response = await openai.completions.create({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p:1,
-            frequency_penalty:0.5,
-            presence_penalty:0,
+            max_tokens: 300,
+           
+            
         })
+        console.log(completion.choices[0].text)
         res.statuus(200).send({
             bot:response.data.choices[0].text
         })
